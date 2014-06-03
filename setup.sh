6f8b76216e6f5604168ac91e50b9a87f9bf535c5
Peter Jerold Leslie <leslie@charlietech.com>
2014-06-03 17:02:58 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1400,6 +1400,6 @@ if [ -n "$INSTALL_GIT" ] ; then
 	   esac
    done
 fi
-    
+echo "Deployment successfully completed..."
 # BASE_DIR=$(cd $(dirname $0);  pwd -P)
 # ansible-playbook $BASE_DIR/helloworld_local.yaml -i $BASE_DIR/inventoryfile
