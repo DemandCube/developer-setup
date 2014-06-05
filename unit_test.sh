997287b73db15bfcb8cd0a638877e5a92bb3ee45
Peter Jerold Leslie <leslie@charlietech.com>
2014-06-05 12:37:05 +0530

    Update unit_test.sh

--- unit_test.sh
+++ unit_test.sh
@@ -2,7 +2,7 @@
 expect -c 'set timeout 6000
 spawn bash /home/developersetup/setup.sh
 puts "Developer Setup Started"
-expect "[Y/n]" { send "y\r" exp_continue}
+expect "\[Y/n]" { send "y\r" exp_continue}
 #expect "INSTALLED" {
 #        if { $expect_out(0,string) == "INSTALLED" } {
 #                puts "$expect_out(buffer) PASS";
