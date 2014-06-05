d18505c094cc40d121c8f472745f02325f0000d4
Peter Jerold Leslie <leslie@charlietech.com>
2014-06-05 15:04:44 +0530

    Update unit_test.sh

--- unit_test.sh
+++ unit_test.sh
@@ -1,36 +1,31 @@
 #!/bin/sh
-expect -c 'set timeout 6000
-spawn bash /home/developersetup/setup.sh
-puts "Developer Setup Started"
-expect "\[y|Y/n|N]" { 
-  send "y\r";
-  exp_continue;
-}
-#expect "INSTALLED" {
-#        if { $expect_out(0,string) == "INSTALLED" } {
-#                puts "$expect_out(buffer) PASS";
-#        } else  {
-#                puts "FAIL";
-#        } 
-#}'
+
 
 command -v git --version >/dev/null 2>&1
 INSTALLED=$?
 echo ""
 
 if [ $INSTALLED == 0 ] ; then
-    #  Git is installed
-    # git version 1.7.4.4
     VERSION_GIT=`git --version | awk '{print $3}'`
-    
     echo "PASS: [Git $VERSION_GIT]"
-    #printf "\t"
-    #echo "$VERSION_GIT"
-
 else
     # Git is not installed
-    #INSTALL_GIT=1
+    
      echo "FAIL: [Git]"
 fi
+
+
+PIP_VERSION=1.5.4
+command -v pip >/dev/null 2>&1
+INSTALLED=$?
+echo ""
+
+if [ ! $INSTALLED == 0 ] ; then
+    echo "FAIL: [pip]"
+else
+    echo "PASS: [ pip ]"
+    # pip -V  # pip verions only works on 1.4
+fi
+
 
 #/home/developersetup/unit_test.exp
