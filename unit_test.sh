4d126e57185af188d6b442dca1ee72d0f9c74901
Peter Jerold Leslie <leslie@charlietech.com>
2014-06-05 12:28:05 +0530

    Update unit_test.sh

--- unit_test.sh
+++ unit_test.sh
@@ -1,20 +1,15 @@
 #!/bin/sh
-chmod 777 /home/developersetup/unit_test.exp
-chmod +x /home/developersetup/bootstrap/os_meta_info.sh
-
 expect -c 'set timeout 6000
 spawn bash /home/developersetup/setup.sh
 puts "Developer Setup Started"
-#expect "\[Y/n]" { send "y\r" }
-
-expect "INSTALLED" {
-        if { $expect_out(0,string) == "INSTALLED" } {
-                puts "$expect_out(buffer) PASS";
-        } else  {
-                puts "FAIL";
-        } 
-}
-#expect "Deployment successfully completed..." {send "\r"}
+expect "\[Y/n]" { send "y\r" exp_continue}
+#expect "INSTALLED" {
+#        if { $expect_out(0,string) == "INSTALLED" } {
+#                puts "$expect_out(buffer) PASS";
+#        } else  {
+#                puts "FAIL";
+#        } 
+#}
 interact'
 
 #/home/developersetup/unit_test.exp
